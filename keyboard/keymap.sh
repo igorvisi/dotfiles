4992d109113a36de0eb249d81e6d25eb2c731e12
igorvisi <salut.visi@gmail.com>
2018-06-28 17:17:40 +0100

    Remap caps to ctrl
    Signed-off-by: igorvisi <salut.visi@gmail.com>

--- keyboard/keymap.sh
+++ keyboard/keymap.sh
@@ -4,12 +4,11 @@
 killall xcape
 
 # Maps caps lock key to super 
-setxkbmap -layout fr -option caps:super
+setxkbmap -layout fr -option ctrl:nocaps
 
 # remap japanese keys to Ctrl+c and Ctrl+v : copy and paste
 # remap Shift Right to Ctrl+ w : close tabs
-# When caps is pressed only once, treat it as escape.
-xcape -e 'Super_L=Escape;Henkan_Mode=Control_L|C;Hiragana_Katakana=Control_L|V;Shift_R=Control_L|W'
+xcape -e 'Henkan_Mode=Control_L|C;Hiragana_Katakana=Control_L|V;Shift_R=Control_L|W'
 
 # remap japanese key to <>  
 xmodmap -e 'keycode 132 = 0x3c 0x3e'
