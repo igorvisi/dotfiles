1a899d9fcefd6c071afaa839e813ef50982cc652
igorvisi <mail@igorvisi.com>
2019-06-16 17:01:24 +0100

    Replace script for install packages  by ansible
    Signed-off-by: igorvisi <mail@igorvisi.com>

--- packages/archlinux.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-pacman -Sy tmux cheat zsh nvim git tig fzf wget exa trash-cli htop neofetch
--- packages/ubuntu.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-apt install tmux cheat zsh nvim git tig fzf wget exa trash-cli htop neofetch
