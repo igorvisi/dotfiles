36e8dc429a0c710a133f31dfb490a07d645708af
Igor Visi <igorvisi@icloud.com>
2024-06-22 13:59:14 +0100

    add new script
    Signed-off-by: Igor Visi <igorvisi@icloud.com>

--- linux/install.sh
+++ linux/install.sh
@@ -21,6 +21,7 @@ zoxide
 fonts-firacode
 python3-pip
 ruby
+
 )
 
 sudo apt install ${APPS[@]}
@@ -29,8 +30,63 @@ sudo apt install ${APPS[@]}
 curl --proto '=https' -fLsS https://rossmacarthur.github.io/install/crate.sh \
     | bash -s -- --repo rossmacarthur/sheldon --to ~/.local/bin
 
+
+
+
 # Install mackup for backup/restore conf
 pip install --upgrade mackup
 
 # Install Jetbrains font
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
+
+
+# Install 1password
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+
+
+
+# Add TablePlus gpg key
+wget -qO - https://deb.tableplus.com/apt.tableplus.com.gpg.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/tableplus-archive.gpg > /dev/null
+
+# Add TablePlus repo
+sudo add-apt-repository "deb [arch=arm64] https://deb.tableplus.com/debian/24-arm tableplus main"
+
+# Install
+sudo apt update
+sudo apt install tableplus
+
+
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list
+
+sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+
+sudo apt update && sudo apt install 1password
+
+# (from https://github.com/basecamp/omakub/)
+
+## Localsend
+cd /tmp
+LOCALSEND_VERSION=$(curl -s "https://api.github.com/repos/localsend/localsend/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+wget -O localsend.deb "https://github.com/localsend/localsend/releases/latest/download/LocalSend-${LOCALSEND_VERSION}-linux-x86-64.deb"
+sudo apt install -y ./localsend.deb
+rm localsend.deb
+cd -
+
+## VSCODE
+cd /tmp
+wget -O code.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'
+sudo apt install -y ./code.deb
+rm code.deb
+cd -
+
+## Ulauncher
+sudo add-apt-repository universe -y
+sudo add-apt-repository ppa:agornostal/ulauncher -y
+sudo apt update -y
+sudo apt install -y ulauncher
+
+
+# Extensions
--- macos/install.sh
+++ macos/install.sh
@@ -4,8 +4,6 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew tap homebrew/cask-fonts
-brew install --cask font-jetbrains-mono
-
 
 # Install my very minimal must tools for MacOS !
 APPS=(
@@ -29,6 +27,27 @@ fzy
 fzf
 git-delta
 zoxide
+docker
+btop
 )
 
 brew install ${APPS[@]}
+
+
+
+APP_DESK=(
+1password
+obsidian
+todoist
+beeper
+readdle-spark
+vlc
+raycast
+iterm2
+font-jetbrains-mono
+tableplus
+brave-browser
+firefox
+microsoft-edge
+)
+brew install --cask ${APPS_DESK[@]}
