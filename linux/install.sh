e14f2f000b6890980b0dcdd46283dbefdb446eee
Igor Visi <igorvisi@icloud.com>
2024-08-26 10:31:11 +0100

    correct vim plug install script

--- linux/install.sh
+++ linux/install.sh
@@ -94,3 +94,8 @@ sudo apt install -y ulauncher
 
 
 # Extensions
+
+
+
+# Vim Plug
+sh -c 'curl -fLo \"${XDG_DATA_HOME:-$HOME/.local/share}\"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
